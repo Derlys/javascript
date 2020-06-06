ca877e55d1d8c544065bb0eaeb2de8e18f62fe1d
Derlys Dominguez <derlys275@gmail.com>
2020-06-05 19:50:37 -0500

    create index

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById('formTask').addEventListener('submit', saveTask);
+
+function saveTask(e) {
+ let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value
+
+    const task = {
+        title,
+        description
+    };
+    console.log(task)
+    e.preventDefault();
+}
